e6e1f25df49bd5aaf633d74f2d33f68da8d7fb03
Stephen Balogh <sgbalogh@gmail.com>
2016-03-04 17:28:27 -0500

    adds beginnings of processor logic (unsecure)

---
+++ app/controllers/processor_controller.rb
@@ -0,0 +1,46 @@
+class ProcessorController < ApplicationController
+  def pursue
+    if !current_user.nil?
+      if is_admin?(current_user)
+        flash[:success] = "Processing thread initiated..."
+        redirect_to datasets_index_path
+        path = 'storage/dataset/multiattach/' + params[:id] + '/' + params[:basename] + '.' + params[:extension]
+        id = get_uuid(params[:id])
+        @good = stage(path, id)
+        unzip(id)
+      else
+        flash[:danger] = "Nice try."
+        redirect_to datasets_index_path
+      end
+    else
+      flash[:danger] = "Nice try."
+      redirect_to datasets_index_path
+    end
+
+
+  end
+
+  private
+
+  def stage(path, id)
+    `cp #{ENV['ROOT']}/#{path} #{ENV['QUARANTINE_DIR']}/#{id}.zip`
+  end
+
+  def unzip(id)
+    `unzip #{ENV['QUARANTINE_DIR']}/#{id}.zip -d #{ENV['QUARANTINE_DIR']}/#{id}`
+  end
+
+  def get_uuid(datasetId)
+    dataset = Dataset.find(datasetId)
+    record = Record.find(dataset.record_id)
+    if record.uuid.nil?
+      return record.id
+    else
+      return record.uuid
+    end
+
+
+  end
+
+
+end
